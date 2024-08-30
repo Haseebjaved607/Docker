3cfea43a4ebafb8f8f1fc577d75cd4398543ffd8
Haseeb Javed <hasiimughal41@gmail.com>
2024-08-30 20:09:44 +0500

    running docker and pushing

---
+++ My-node-app/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(9000, () => {
+  console.log(`Server is running on port http://localhost:9000`);
+});
